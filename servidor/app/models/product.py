508788e526393724925a874eea9adeb344ab894b
Tomas Mattioli <tomasmattioli29@gmail.com>
2026-09-14 20:25:06 -0300

    Agregar Alembic y primera migracion (tabla products)

--- servidor/app/models/product.py
+++ servidor/app/models/product.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Colum, Integer, String, Numeric
+from sqlalchemy import Column, Integer, String, Numeric
 from app.database import Base
 
 class Product(Base):
@@ -7,8 +7,8 @@ class Product(Base):
     id = Column(Integer, primary_key = True, index = True)
     name = Column(String(100), nullable = False)
     price = Column(Numeric(10, 2), nullable = False)
-    stock = Colum(Numeric(10, 3), nullable = False, default = 0)
-    barcode = Colum(String(50), unique = True, nullable = True, index = True)
+    stock = Column(Numeric(10, 3), nullable = False, default = 0)
+    barcode = Column(String(50), unique = True, nullable = True, index = True)
     
 def __repr__(self):
     return f"<Product {self.name} - stock_ {self.stock}>"
