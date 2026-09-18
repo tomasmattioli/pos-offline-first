64f4e54147baf7ca19bfc154cc81079fdbf338f7
Tomas Mattioli <tomasmattioli29@gmail.com>
2026-09-18 18:02:52 -0300

    Agregar CRUD completo de products (GET, PUT, DELETE)

--- servidor/app/routers/products.py
+++ servidor/app/routers/products.py
@@ -13,3 +13,43 @@ def create_product(product: ProductCreate, db:Session = Depends(get_db)):
     db.commit()
     db.refresh(new_product)
     return new_product
+
+@router.get("/products/{product_id}", response_model=ProductOut)
+def get_product(product_id: int, db: Session= Depends(get_db)):
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if not product:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Producto con ID {product_id} no encontrado"
+        )
+
+    return product
+
+
+@router.put("/product/{product_id}", response_model=ProductOut)
+def update_product(product_id: int,update_data:ProductCreate, db: Session = Depends(get_db)):
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if not product:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Prodcuto con ID {product_id} no encontrado"
+        )
+    product.name = update_data.name
+    product.price = update_data.price
+    product.stock = update_data.stock
+    product.barcode = update_data.barcode
+    db.commit()
+    db.refresh(product)
+    return product
+
+@router.delete("/product/{product_id}", response_model=ProductOut)
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if not product:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Prodcuto con ID {product_id} no encontrado"
+        )
+    db.delete(product)
+    db.commit()
+    return product
